64b85b2b1b43434574af281048891ba5fa5d42a9
Epi-Yeti <77855700+TheMemeticist@users.noreply.github.com>
2022-04-12 16:34:04 -0600

    Update linkComp.js
    added video icon to links

--- js/linkComp.js
+++ js/linkComp.js
@@ -1,8 +1,14 @@
 
 
 class Link {
-  constructor(name, link) {
-    let d = "<a href='link'><div class='lcontainer'><img id='limg' src='is'><p>title</p></div></a>";
+  constructor(name, link, video=false) {
+    let d;
+    console.log(video);
+    if (video) {
+      d = "<a href='link'><div class='lcontainer'><div id='playbutton' class='material-icons md-light'>videocam</div><img id='limg' src='is'><p>title</p></div></a>";
+    } else {
+      d = "<a href='link'><div class='lcontainer'><img id='limg' src='is'><p>title</p></div></a>";
+    }
     let il = 'assets/links/' + name + '.png'
     d = d.replace("is", il);
     d = d.replace("'link'", link);
