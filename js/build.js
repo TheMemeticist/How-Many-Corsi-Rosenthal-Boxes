1acad29167fc38dd5d1e40ad5d743b7fe5cd5990
Epi-Yeti <77855700+TheMemeticist@users.noreply.github.com>
2022-04-12 16:33:34 -0600

    Update build.js

--- js/build.js
+++ js/build.js
@@ -2,17 +2,26 @@ import {Link} from './linkComp.js';
 
 
 let buildlinks = {
-  'First time build - James Smith':'https://www.youtube.com/watch?v=aEn2xzlvrdo',
-  'Ask This Old House':'https://www.youtube.com/watch?v=aw7fUMhNov8',
-  'Instructables':'https://www.instructables.com/Efficient-Box-Fan-Air-Cleaner-a-Universal-Corsi-Ro/',
-  'Clean Air Crew':'https://cleanaircrew.org/box-fan-filters/',
+  'Single Filter Air Cleaner':'https://www.youtube.com/watch?v=lEpFeiWwvPI',
+  'Table-Top Box':'https://github.com/661020786/Anemoi',
+  'CR-Thomas Box V1':'https://grabcad.com/library/diy-corsi-rosenthal-thomas-box-v1-20x25x1-inch-filters-1',
+  'CAD Model':'https://grabcad.com/library/diy-corsi-rosenthal-box-20x20x1-inch-filters-1',
+  'First Time Build':'https://www.youtube.com/watch?v=aEn2xzlvrdo',
+  'How to Make a CR-Box':'https://www.youtube.com/watch?v=aw7fUMhNov8',
+  'Universal CR-Box':'https://www.instructables.com/Efficient-Box-Fan-Air-Cleaner-a-Universal-Corsi-Ro/',
+  'DIY Box Fan Air Filters':'https://cleanaircrew.org/box-fan-filters/',
 };
-
+let videos = ['How to Make a CR-Box', 'First Time Build', 'Single Filter Air Cleaner'];
 let links = [];
 let node = document.getElementsByTagName("BODY")[0];
-let d = "<a href='index.html'><div id='backbutt'><div class='material-icons md-light'>arrow_back</div></div><div id='linkscont'></div><a>";
+let d = "<a href='index.html'><div id='backbutt'><div class='material-icons md-light'>home</div></div><a><div id='linkscont'></div>";
 node.insertAdjacentHTML('afterbegin', d);
 for (let key in buildlinks) {
-  let l = new Link(key, buildlinks[key]);
+  let l;
+  if (videos.includes(key)) {
+    l = new Link(key, buildlinks[key], true);
+  } else {
+    l = new Link(key, buildlinks[key]);
+  }
   links.push(l);
 };
