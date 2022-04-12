3adf9f6556797ba0d377739a986a80cfc76d1d42
Epi-Yeti <77855700+TheMemeticist@users.noreply.github.com>
2022-04-12 16:33:07 -0600

    Update order.js

--- js/order.js
+++ js/order.js
@@ -3,15 +3,18 @@
 import {Link} from './linkComp.js';
 
 let buylinks = {
-  'Clean AR Air':'https://cleanarair.com/',
-  'Clean Air Crew':'https://cleanaircrew.org/air-cleaners/',
+  // ships to UK, Australia, Canada, New Zealand, Malaysia,
+  // South Africa, USA,
   'Mini CR-Box':'https://www.texairfilters.com/mini-corsi-rosenthal-box-filter-kit/',
+  'Clean Air Crew':'https://cleanaircrew.org/air-cleaners/',
   'Zero COVID Canada':'https://docs.google.com/presentation/d/150_1ImNcnlaKI3DElDcHfO7sVkSW_4_Anm76O9BHw0E/edit#slide=id.p',
+  'Clean AR Air':'https://cleanarair.com/',
+  'Clean Air Stars':'https://cleanairstars.com/hepafilters/',
 };
 
 let links = [];
 let node = document.getElementsByTagName("BODY")[0];
-let d = "<a href='index.html'><div id='backbutt'><div class='material-icons md-light'>arrow_back</div></div><div id='linkscont'></div><a>";
+let d = "<a href='index.html'><div id='backbutt'><div class='material-icons md-light'>home</div></div><div id='linkscont'></div><a>";
 node.insertAdjacentHTML('afterbegin', d);
 for (let key in buylinks) {
   let l = new Link(key, buylinks[key]);
